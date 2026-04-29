1c63b442599ff190194f1d33c8aaba8b1fe6aba2
lourdes247 <luluffll13@gmail.com>
2026-04-28 23:18:54 -0400

    Tercer commit: mejora en validación

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
                 String input = etN.getText().toString().trim();
 
                 if (input.isEmpty()) {
-                    etN.setError("no lo dejes vacío");
+                    etN.setError("no lo dejes vacío >:v");
                     return;
                 }
 
